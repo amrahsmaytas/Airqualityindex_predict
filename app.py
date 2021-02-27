0219f8a2545789e4950afdba7d305264c93593a9
satyamsharma <satyamsharma211999@gmail.com>
2021-02-28 00:42:58 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,45 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template, url_for
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('randomForestRegressor.pkl','rb'))
+
+@app.route('/')
+def home():
+    #return 'Hello World'
+    return render_template('home.html')
+    #return render_template('index.html')
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    print(prediction[0])
+    output = round(prediction[0],1) #gives output
+    print(output)
+    if output in range(0,51):
+      return render_template('good_home.html', prediction_text="Air Quality Index is: {}".format(output) #give whole output+text
+    if output in range(51,100):
+      return render_template('moderate_home.html', prediction_text="Air Quality Index is: {}".format(output) #give whole output+text
+    if output in range(101,150):
+      return render_template('Unhealthy_for_Sensitive_Groups.html', prediction_text="Air Quality Index is: {}".format(output) #give whole output+text
+    if output in range(151,200):
+      return render_template('Unhealthy_result.html', prediction_text="Air Quality Index is: {}".format(output) #give whole output+text
+    if output in range(201,300):
+      return render_template('Very_Unhealthy.html', prediction_text="Air Quality Index is: {}".format(output)#give whole output+text
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
