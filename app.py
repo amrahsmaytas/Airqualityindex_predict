5776ef3369dd17c803136d34b52a6de324e36647
satyamsharma <satyamsharma211999@gmail.com>
2021-02-28 00:05:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template, url_for
 import pickle
 
-
 app = Flask(__name__)
 model = pickle.load(open('randomForestRegressor.pkl','rb'))
-
 
 @app.route('/')
 def home():
@@ -21,18 +19,15 @@ def predict():
     print(prediction[0])
     output = round(prediction[0], 2) #gives output
     if output in range(0,51):
-        return render_template('good_home.html', prediction_text="Air Quality Index is: {}".format(output) #give whole output+text
+       return render_template('good_home.html', prediction_text="Air Quality Index is: {}".format(output) #give whole output+text
     elif output in range(51,100):
-        return render_template('moderate_home.html', prediction_text="Air Quality Index is: {}".format(output) #give whole output+text
+         return render_template('moderate_home.html', prediction_text="Air Quality Index is: {}".format(output) #give whole output+text
     elif output in range(101,150):
-        return render_template('Unhealthy_for_Sensitive_Groups.html', prediction_text="Air Quality Index is: {}".format(output) #give whole output+text
+         return render_template('Unhealthy_for_Sensitive_Groups.html', prediction_text="Air Quality Index is: {}".format(output) #give whole output+text
     elif output in range(151,200):
-        return render_template('Unhealthy_result.html', prediction_text="Air Quality Index is: {}".format(output) #give whole output+text
+         return render_template('Unhealthy_result.html', prediction_text="Air Quality Index is: {}".format(output) #give whole output+text
     else:
-        return render_template('Very_Unhealthy.html', prediction_text="Air Quality Index is: {}".format(output)#give whole output+text
-
-
-
+         return render_template('Very_Unhealthy.html', prediction_text="Air Quality Index is: {}".format(output)#give whole output+text
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
@@ -44,8 +39,6 @@ def predict_api():
 
     output = prediction[0]
     return jsonify(output)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
